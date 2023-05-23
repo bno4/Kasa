56ea099d6b0b738cb1cfed7e9f4ad679c491247a
bno4 <bnduchemin@gmail.com>
2023-05-23 15:17:45 +0200

    install create-react, react-router-dom

---
+++ src/pages/About.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
+        temporibus magnam officiis quisquam neque eum eveniet blanditiis
+        dignissimos consequuntur ipsum, qui sequi, ipsam quidem ex reprehenderit
+        ducimus soluta, iste veritatis provident molestiae harum ab. Optio
+        eveniet ad deleniti unde architecto a vero maiores fugit, vel pariatur
+        dolor odio omnis perferendis consectetur incidunt qui ex nemo quis.
+        Nesciunt ex qui porro libero placeat ullam ipsam culpa sequi similique
+        ratione harum vitae distinctio, esse adipisci est nulla, blanditiis
+        itaque. Voluptatem, nostrum. Ad nisi fuga cupiditate esse incidunt
+        tempora facere quia, officia autem sed, et dolore, nobis aliquid qui at
+        quis! Voluptatum sapiente odio tenetur quibusdam iusto minima dolorum
+        totam. Quibusdam atque pariatur distinctio ea, reiciendis ratione
+        deserunt ad? Provident nam eius quo sapiente beatae suscipit odit
+        dignissimos hic laborum nisi, non a repudiandae, aut placeat fugit animi
+        ullam expedita libero fugiat ad nostrum rem? Tempore eos blanditiis,
+        officia repellat quas veniam. Perspiciatis quis sed reiciendis, nostrum
+        distinctio culpa harum adipisci quasi quo rem voluptate? Veniam
+        molestias provident magnam vero, enim, tempora inventore aut nam quae
+        fugit ipsum amet magni iure, laboriosam vel temporibus odio! Deleniti
+        reiciendis fuga voluptatem omnis nihil ut laboriosam minus, quae
+        asperiores? Voluptatibus ex dolor eaque suscipit, deserunt debitis.
+      </p>
+    </div>
+  )
+}
+
+export default About
