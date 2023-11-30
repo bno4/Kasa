45058eafb1402cadd0d461c98b7eb6f61ef10893
bno4 <bnduchemin@gmail.com>
2023-11-30 15:28:19 +0100

    deploy website with harshRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Error from './pages/Error'
@@ -7,14 +7,14 @@ import Place from './pages/Place'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/place/:id" element={<Place />}></Route>
         <Route path="*" element={<Error />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
